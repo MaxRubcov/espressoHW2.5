8fc5b720b32f22ad29f487154c3b27f9cced00ba
MaksRubcov <m_rubcov@bk.ru>
2024-12-02 01:03:46 +0300

    add IdlingTest

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/IdlingTest.java
@@ -0,0 +1,87 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.AllOf.allOf;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+
+@LargeTest
+@RunWith(AllureAndroidJUnit4.class)
+public class IdlingTest {
+
+    @Rule
+    public IntentsTestRule<MainActivity> mActivityScenarioRule =
+            new IntentsTestRule<>(MainActivity.class);
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+
+    @Before
+    public void registerIdlingResources() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResources.idlingResource);
+    }
+
+    @After
+    public void unregisterIdlingResources() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResources.idlingResource);
+    }
+
+    @Test
+    public void idlingTest() {
+        ViewInteraction menu = onView(isAssignableFrom(AppCompatImageButton.class));
+        menu.check(matches(isDisplayed()));
+        menu.perform(click());
+
+        ViewInteraction gallery = onView(withId(R.id.nav_gallery));
+        gallery.perform(click());
+
+        ViewInteraction recyclerView = onView(withId(R.id.recycle_view));
+        recyclerView.check(matches(CustomViewMatcher.recyclerViewSizeMatcher(10)));
+        recyclerView.check(CustomViewAssertions.isRecyclerView());
+
+        ViewInteraction itemSeven = onView(allOf(withId(R.id.item_number), withText("7")));
+        itemSeven.check(matches(withText("7")));
+    }
+}
